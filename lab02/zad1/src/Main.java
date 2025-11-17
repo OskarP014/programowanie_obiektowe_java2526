b4cf7fa553d1ba792ee32212aadbb1953d4fb2af
OskarP014 <184099574+OskarP014@users.noreply.github.com>
2025-11-17 01:46:29 +0100

    Add files via upload

---
+++ lab02/zad1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void pierwsza(int n) {
+            Scanner scanner = new Scanner(System.in);
+            if(n<=2){
+                System.out.printf("Podana liczba musi byc wieksza niz 2");
+            }else {
+                for(int i=0;)
+            }
+        }
+    }
